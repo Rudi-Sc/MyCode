54ac7e65531f5e568ad5358d5ae4530302698e10
Rudi <rodins@rafael.co.il>
2026-03-01 08:48:48 +0200

    fixes

--- my_server.py
+++ my_server.py
@@ -1,12 +1,19 @@
 # https://gofastmcp.com/getting-started/quickstart
 
 from fastmcp import FastMCP
+import math
+
 # Instantiate the server
 mcp = FastMCP("My MCP Server")
 
 @mcp.tool
 def greet(name: str) -> str:
     return f"Hello, {name}!"
+
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return int(a + b)
 
 if __name__ == "__main__":
     mcp.run()
